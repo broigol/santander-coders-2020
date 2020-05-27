5065c97f44be56a5b55a4c37c7ed586bd8e5904d
Igor Romano <broigor@hotmail.com>
2020-05-26 21:53:49 -0300

    Segunda etapa desafio 'Professora Furiosa'

--- professoraFuriosa.js
+++ professoraFuriosa.js
@@ -1,3 +1,8 @@
+/* 
+Escreva a funcão "acontece", que diz se a aula sucederá de acordo com o array dos estudantes que entraram. 
+Ex.: acontece(alunosDaSegunda, 2) => true
+*/
+
 function acontece (alunosDaSegunda,minimo) {
     var aula = 0;
     for (let i = 0; i < alunosDaSegunda.length; i++) {
@@ -9,5 +14,43 @@ function acontece (alunosDaSegunda,minimo) {
     return aula >= minimo; 
 }; 
 acontece([-2,0,0,15],2);
+
+/* 
+Escreva a função "aberturas", que utiliza um array com os arrays dos estudantes que entraram nos outros dias,
+e a quantidade mínima de estudantes, e diga quais os dias em que as aulas aconteceram e quais não. 
+Ex.: aberturas([alunosDaSegunda, alunosDaTerça, alunosDaQuarta], 2) => [true, false, false]
+*/
+
+function aberturas (alunosDaSegunda, alunosDaTerca, alunosDaQuarta, minimo) {
+    var aulas = [];
+    var aulaSegunda = 0;
+    var aulaTerca = 0;
+    var aulaQuarta = 0;
+    //Aula de segunda:  
+    for (let i = 0; i < alunosDaSegunda.length; i++) {
+        if (alunosDaSegunda[i] <=0 ) {
+            aulaSegunda = aulaSegunda + 1; 
+        }
+    };
+    aulas.push(aulaSegunda >= minimo);
+    
+    //Aula de Terça:
+    for (let i = 0; i < alunosDaTerca.length; i++) {
+        if (alunosDaTerca[i] <=0 ) {
+            aulaTerca = aulaTerca + 1; 
+        }
+    };
+    aulas.push(aulaTerca >= minimo);
+    
+    //Aula de Quarta:
+    for (let i = 0; i < alunosDaQuarta.length; i++) {
+        if (alunosDaQuarta[i] <=0 ) {
+            aulaQuarta = aulaQuarta + 1; 
+        }
+    };
+    aulas.push(aulaQuarta >= minimo);
+    return aulas; 
+}; 
+aberturas([-2,0,0,15], [-2,2,10,15], [-2,1,0,15], 2);
 
 
